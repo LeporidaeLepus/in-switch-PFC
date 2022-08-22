baf0c3a0e930ffbeb6027be423ddaf7303d57654
LeporidaeLepus <jingyuwang0499@gmail.com>
2022-08-22 17:56:57 -0400

    test

--- TOPO.cpp
+++ TOPO.cpp
@@ -1,59 +1,63 @@
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
-#include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
-#include "ns3/ipv4-global-routing-helper.h"
 
-using namespace NS3;
+// Default Network Topology
+//
+//       10.1.1.0
+// n0 -------------- n1
+//    point-to-point
+//
 
-NS_LOG_COMPONENT_DEFINE ("MyTopo");
-
-const int nSERVER = 3;
-const int nCLIENT = 1;
-const int nROUTER = 1;
-
+using namespace ns3;
+ 
+NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+ 
 int
-mian (int argc, char *argv[])
-{
-    bool verbose = true;
-    
-    CommandLine cmd;
+main (int argc, char *argv[]){
+    CommandLine cmd (__FILE__);
     cmd.Parse (argc, argv);
+  
     Time::SetResolution (Time::NS);
+    LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+ 
+    NodeContainer nodes;
+    nodes.Create (3);
+ 
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+    pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+ 
+    NetDeviceContainer devices;
+    devices = pointToPoint.Install (nodes);
+ 
+    InternetStackHelper stack;
+    stack.Install (nodes);
+ 
+    Ipv4AddressHelper address;
+    address.SetBase ("10.1.1.0", "255.255.255.0");
+ 
+    Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
-    // if (verbose)
-    // {
-    //     LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    //     LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
-    // }
+    UdpEchoServerHelper echoServer (9);
 
+    ApplicationContainer serverApps = echoServer.Install (nodes.Get (2));
+    serverApps.Start (Seconds (1.0));
+    serverApps.Stop (Seconds (10.0));
 
-}
+    UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+    echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+    echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+    echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-
-
-static void
-RxDrop(Ptr<const Packet> packet) {
-    /* 
-     * Need to copy packet since headers need to be removed
-     * to be inspected. Alternatively, remove the headers,
-     * and add them back.
-     */
-    Ptr<Packet> copy = packet->Copy();
-
-    // Headers must be removed in the order they're present.
-    PppHeader pppHeader;
-    copy->RemoveHeader(pppHeader);
-    Ipv4Header ipHeader;
-    copy->RemoveHeader(ipHeader);
-
-    std::cout << "Source IP: ";
-    ipHeader.GetSource().Print(std::cout);
-    std::cout << std::endl;
-
-    std::cout << "Destination IP: ";
-    ipHeader.GetDestination().Print(std::cout);
-    std::cout << std::endl;
+    ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+    clientApps.Start (Seconds (2.0));
+    clientApps.Stop (Seconds (10.0));
+    
+    Simulator::Run ();
+    Simulator::Destroy ();
+    return 0;
 }
